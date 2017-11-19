eb14e6651ad021e0843ebda06d635b9d4199c5f2
Dillon Andreas <Dillon.Andreas@cis.umary.edu>
2017-11-18 18:17:50 -0600

    Initial commit

---
+++ autobackup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Autobackup.py Main Module
+
+This module is the entrypoint and controls all functions of autobackup.py.
+This application can both upload a backup to a server, and automatically can install a list of applications
+to your hard disk.
+
+Attributes:
+    
+Todo:
+    Everything.
+"""
+import os
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+#AutoBackup.py
+
+This application will automatically install apps on a linux operating system, and will be able to sync backups to a remote server.
+
+Currently, this app is in active development and will not work, or if it does, it won't have all of the features above.
+
+Features currently added:
+- nothing
